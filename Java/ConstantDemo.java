f28091232ea7b7b00b1e9056f2579f60bf0b4ba7
wsh123456789 <2407202106@qq.com>
2022-01-14 21:23:52 +0800

    学习基础第一天

---
+++ Java/ConstantDemo.java
@@ -0,0 +1,31 @@
+/*
+    常量：
+        在运行过程中，其值不会发生改变的量。
+    常量分类：
+        字符串常量： 用双引号括起来，"helloworld" ,"你好啊"。
+        整数常量
+        小数常量
+        字符常量：  用单引号，‘A’,‘我’.
+        布尔常量：  布尔值，表示真假。true,false
+        空常量：   一个特殊的值，空值，null
+ */
+public class ConstantDemo {
+    public static void main(String[] args){
+        // 字符串常量
+        System.out.println("HelloWorld");
+        System.out.println("你好啊");
+        // 整数常量
+        System.out.println("666");
+        System.out.println("-88");
+        // 小数常量
+        System.out.println(13.14);
+        System.out.println(-5.21);
+        // 字符常量
+        System.out.println('A');
+        System.out.println('我');
+        // 布尔常量
+        System.out.println(true);
+        System.out.println(false);
+        // 空常量不能直接输出
+    }
+}
---
+++ Java/helloword.java
@@ -0,0 +1,19 @@
+/*
+    java程序中最基本的组成单位是类
+
+    类的定义格式：
+    {
+        public class 类名{
+
+    }
+ */
+public class helloword {
+    /*
+        这是main方法
+        main方法是程序的入口方法，代码的执行是从main方法开始的
+     */
+    public static void main(String[] args){
+        // 这是输出语句，“ ” 里边的内容是可以改变的
+        System.out.println("Hello Word!");
+    }
+}
